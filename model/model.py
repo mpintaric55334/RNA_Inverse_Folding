21867e80ec06e8a0c5d448453e2399ebedbdc59e
mpintaric55334 <pintamato@gmail.com>
2024-04-29 16:01:16 +0000

    Separated Encoder class

--- model/model.py
+++ model/model.py
@@ -8,14 +8,8 @@ class RNAModel(nn.Module):
     def __init__(self, matrix_shape:int = 512):
         
         super(RNAModel, self).__init__()
-        self.blocks = []
-        for i in range(5):
-            self.blocks.append(ResNetBlock(in_channels=2**i,out_channels=2**(i+1),kernel_size=3))
-            matrix_shape = int(matrix_shape/2)
-
-        self.blocks.append(FCLayer(in_channels=2**(i+1),shape_x=matrix_shape,shape_y=matrix_shape))
+        self.encoder = Encoder()
 
     def forward(self,x):
-        for block in self.blocks:
-            x = block(x)
+        x = self.encoder(x)
         return x
--- model/modules.py
+++ model/modules.py
@@ -84,3 +84,27 @@ class FCLayer(nn.Module):
     def forward(self,x):
         x = self.fc1(self.flatten(x))
         return x
+
+
+class Encoder(nn.Module):
+    """
+    Encoder(resnet) class.
+
+    Arguments:
+        matrix_shape: input matrix shape
+
+    """
+    def __init__(self, matrix_shape:int = 512):
+        
+        super(Encoder, self).__init__()
+        self.blocks = []
+        for i in range(5):
+            self.blocks.append(ResNetBlock(in_channels=2**i,out_channels=2**(i+1),kernel_size=3))
+            matrix_shape = int(matrix_shape/2)
+
+        self.blocks.append(FCLayer(in_channels=2**(i+1),shape_x=matrix_shape,shape_y=matrix_shape))
+
+    def forward(self,x):
+        for block in self.blocks:
+            x = block(x)
+        return x
